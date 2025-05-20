f770e189e0f56f745fd388838431a7bf629bf317
shazy91 <phenriquems91@gmail.com>
2025-05-20 02:35:42 +0000

    Add Tailwind config with Lava and Basalto palette

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lava: '#cf1020',
+        basalto: '#606060'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
